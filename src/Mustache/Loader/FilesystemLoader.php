d8954651c1b08ed1c8584557445a6f18203d1297
Chris Heng <bigblah@gmail.com>
2012-12-25 00:27:08 +0800

    Support custom directory prefixes

--- src/Mustache/Loader/FilesystemLoader.php
+++ src/Mustache/Loader/FilesystemLoader.php
@@ -29,7 +29,7 @@
 class Mustache_Loader_FilesystemLoader implements Mustache_Loader
 {
     private $baseDir;
-    private $extension = '.mustache';
+    protected $extension = '.mustache';
     private $templates = array();
 
     /**
